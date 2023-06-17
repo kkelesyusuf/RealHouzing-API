25b4a2a7e86c629c7cb8944ce49c1d6427f6f92d
kkelesyusuf <kkelesyusuf@gmail.com>
2023-06-17 17:22:37 +0300

    Add project files.

---
+++ RealHouzing.ApiLayer/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealHouzing.BusinessLayer.Abstract;
+
+namespace RealHouzing.ApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        //-----------------------------------------------------------------------
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetList();
+            return Ok(values);
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id) 
+        {
+            var value = _categoryService.TGetById(id);
+            _categoryService.TDelete(value);
+            return Ok();
+        }
+    }
+}
---
+++ RealHouzing.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealHouzing.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealHouzing.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-MM84V2T\\SQLEXPRESS;initial catalog=DBRealHouzingApi;integrated security=true");
+        }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Product> Products { get; set; }
+    }
+}
